60c4fb0cc8da494936e3affbb4520e722dc0ae35
reza72ahmadi <reza72.ahmadi@gmail.com>
2024-11-06 20:35:19 +0430

    added

---
+++ database/migrations/2024_11_06_154410_create_ideas_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ideas', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->text('like');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ideas');
+    }
+};
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,27 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://bootswatch.com/5/sketchy/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/css/all.css') }}">
+    <script src="{{ asset('bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('jQuery.js') }}"></script>
+</head>
+@include('layout.nav')
+
+<body>
+
+
+    @yield('content')
+
+</body>
+
+</html>
